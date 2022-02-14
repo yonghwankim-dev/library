ad1bb872b9450e4a157205a19aa129728ddfe188
yonghwan <yonghwankim.dev@gmail.com>
2022-02-15 00:12:22 +0900

    mapper interface

--- src/com/yh/libraryapp/config/MemberMapper.java
+++ src/com/yh/libraryapp/config/MemberMapper.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.annotations.Select;
 
 import com.yh.libraryapp.member.model.vo.MemberLoginVO;
 
+
 public interface MemberMapper {
 	@Select("SELECT * FROM member WHERE mem_email = #{mem_email}")
 	MemberLoginVO login(String mem_email);
